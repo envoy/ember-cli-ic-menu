c1cd0f0c4fc2d65192c932441cf8afd3cbb1dd20
Kamal Mahyuddin <kamal.fariz@gmail.com>
2014-12-10 10:59:38 -0800

    import ic-menu

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-ic-menu'
+  name: 'ember-cli-ic-menu',
+
+  treeFor: function(name){
+    if (name !== 'app') { return; }
+    return this.treeGenerator(path.join(__dirname, 'node_modules/ic-menu/lib'));
+  }
 };
